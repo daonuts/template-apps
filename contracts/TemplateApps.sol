386903ec1e8905c0513bb4402bbb5498a122d076
Carl Larson <cslarson@gmail.com>
2019-10-25 20:09:37 -0400

    use delegatecall, open permissions

--- contracts/TemplateApps.sol
+++ contracts/TemplateApps.sol
@@ -17,86 +17,126 @@ import "@daonuts/tipping/contracts/Tipping.sol";
 contract TemplateApps {
     ENS public ens;
 
-    uint constant TOKEN_UNIT = 10 ** 18;
-    address constant ANY_ENTITY = address(-1);
+    uint TOKEN_UNIT;
+    address ANY_ENTITY;
 
-    //namehash("airdrop-duo-app.open.aragonpm.eth")
-    bytes32 constant airdropDuoAppId = 0xa9e4c5b47fe3f0e61f6a7a045848bf59d44a5eaad3fbb6274929030a2030797d;
-    //namehash("challenge-app.open.aragonpm.eth")
-    bytes32 constant challengeAppId = 0x67c5438d71d05e58f99e88d6fb61ea5356b0f57106d3aba65c823267cc1cd07e;
-    //namehash("subscribe-app.open.aragonpm.eth")
-    bytes32 constant subscribeAppId = 0xb6461185219d266fa4eb5f1acad9b08a010bfd1e1f6a45fe3e169f161d8d5af1;
-    //namehash("tipping-app.open.aragonpm.eth")
-    bytes32 constant tippingAppId = 0x2d550bdd0046ce7aa5f255924dc9665972f04f1563519485689baf371e8d224b;
+    bytes32 airdropDuoAppId;
+    bytes32 challengeAppId;
+    bytes32 subscribeAppId;
+    bytes32 tippingAppId;
 
     event InstalledApp(address appProxy, bytes32 appId);
+    event Debug(address debug);
 
     constructor(ENS _ens) public {
         ens = _ens;
     }
 
     function install(
-        Kernel _dao, Voting _voting, TokenManager _contribManager, TokenManager _currencyManager
+        Kernel _dao, Voting _voting, TokenManager _contribManager, TokenManager _currencyManager,
+        AirdropDuo _airdrop, Challenge _challenge, Subscribe _subscribe, Tipping _tipping
     ) public {
         ACL acl = ACL(_dao.acl());
 
-        AirdropDuo airdrop = AirdropDuo(_dao.newAppInstance(airdropDuoAppId, latestVersionAppBase(airdropDuoAppId)));
-        Challenge challenge = Challenge(_dao.newAppInstance(challengeAppId, latestVersionAppBase(challengeAppId)));
-        Subscribe subscribe = Subscribe(_dao.newAppInstance(subscribeAppId, latestVersionAppBase(subscribeAppId)));
-        Tipping tipping = Tipping(_dao.newAppInstance(tippingAppId, latestVersionAppBase(tippingAppId)));
-
-        airdrop.initialize(_contribManager, _currencyManager);
-        emit InstalledApp(airdrop, airdropDuoAppId);
-        challenge.initialize(
+        _airdrop.initialize(_contribManager, _currencyManager);
+        _challenge.initialize(
           _currencyManager, 100*TOKEN_UNIT, 10*TOKEN_UNIT, 50*TOKEN_UNIT,
           uint64(1 minutes), uint64(1 minutes), uint64(1 minutes)
         );
-        emit InstalledApp(challenge, challengeAppId);
-        subscribe.initialize(_currencyManager, 10000*TOKEN_UNIT, uint64(30 days));
-        emit InstalledApp(subscribe, subscribeAppId);
-        tipping.initialize(_currencyManager.token());
-        emit InstalledApp(tipping, tippingAppId);
+        _subscribe.initialize(_currencyManager, 10000*TOKEN_UNIT, uint64(30 days));
+        _tipping.initialize(_currencyManager.token());
 
-        _permissions(_dao, acl, _voting, _contribManager, _currencyManager, airdrop, challenge, subscribe, tipping);
+        acl.createPermission(_airdrop, _contribManager, _contribManager.MINT_ROLE(), msg.sender);
+        acl.createPermission(_airdrop, _currencyManager, _currencyManager.MINT_ROLE(), msg.sender);
+        acl.createPermission(_challenge, _airdrop, _airdrop.START_ROLE(), msg.sender);
+        acl.createPermission(ANY_ENTITY, _challenge, _challenge.PROPOSE_ROLE(), msg.sender);
+        acl.createPermission(ANY_ENTITY, _challenge, _challenge.CHALLENGE_ROLE(), msg.sender);
+        acl.createPermission(_voting, _challenge, _challenge.SUPPORT_ROLE(), msg.sender);
+        acl.createPermission(ANY_ENTITY, _subscribe, _subscribe.SET_PRICE_ROLE(), msg.sender);
+        acl.createPermission(ANY_ENTITY, _tipping, _tipping.NONE(), msg.sender);
+
+        /* _tokenPermissions(_dao, _voting, _contribManager, _currencyManager);
+        _challengePermissions(_dao, _voting, _currencyManager, challenge);
+        _subscribePermissions(_dao, _voting, _currencyManager, subscribe);
+        _airdropPermissions(_dao, _voting, _contribManager, _currencyManager, airdrop);
+        _cleanup(_dao, _voting, _contribManager, _currencyManager, tipping); */
     }
 
-    function _permissions(
-        Kernel _dao, ACL _acl, Voting _voting, TokenManager _contribManager,
-        TokenManager _currencyManager, AirdropDuo _airdrop, Challenge _challenge,
-        Subscribe _subscribe, Tipping tipping
+    function _tokenPermissions(
+        Kernel _dao, Voting _voting, TokenManager _contribManager, TokenManager _currencyManager
     ) internal {
+        ACL acl = ACL(_dao.acl());
+        bytes32 MINT_ROLE = _contribManager.MINT_ROLE();
+        bytes32 BURN_ROLE = _contribManager.BURN_ROLE();
 
+        acl.createPermission(_contribManager, _voting, _voting.CREATE_VOTES_ROLE(), _voting);
+        acl.createPermission(_voting, _contribManager, BURN_ROLE, _voting);
+        acl.createPermission(this, _contribManager, MINT_ROLE, this);
+
+        address[] memory holders = new address[](1);
+        holders[0] = msg.sender;
+        _mintHolders(_contribManager, holders);
+
+        acl.revokePermission(this, _contribManager, MINT_ROLE);
+    }
+
+    function _challengePermissions(
+        Kernel _dao, Voting _voting, TokenManager _currencyManager, Challenge _challenge
+    ) internal {
+        ACL acl = ACL(_dao.acl());
+
+        acl.createPermission(_challenge, _currencyManager, _currencyManager.MINT_ROLE(), this);
+        acl.createPermission(_challenge, _currencyManager, _currencyManager.BURN_ROLE(), this);
+        acl.createPermission(ANY_ENTITY, _challenge, _challenge.PROPOSE_ROLE(), _voting);
+        acl.createPermission(ANY_ENTITY, _challenge, _challenge.CHALLENGE_ROLE(), _voting);
+        acl.createPermission(_voting, _challenge, _challenge.SUPPORT_ROLE(), _voting);
+        acl.createPermission(_challenge, _challenge, _challenge.MODIFY_PARAMETER_ROLE(), _voting);
+    }
+
+    function _subscribePermissions(
+        Kernel _dao, Voting _voting, TokenManager _currencyManager, Subscribe _subscribe
+    ) internal {
+        ACL acl = ACL(_dao.acl());
+
+        acl.grantPermission(_subscribe, _currencyManager, _currencyManager.BURN_ROLE());
+        acl.createPermission(_voting, _subscribe, _subscribe.SET_PRICE_ROLE(), _voting);
+        acl.createPermission(_voting, _subscribe, _subscribe.SET_DURATION_ROLE(), _voting);
+    }
+
+    function _airdropPermissions(
+        Kernel _dao, Voting _voting, TokenManager _contribManager, TokenManager _currencyManager, AirdropDuo _airdrop
+    ) internal {
+        ACL acl = ACL(_dao.acl());
+
+        acl.grantPermission(_airdrop, _contribManager, _contribManager.MINT_ROLE());
+        acl.grantPermission(_airdrop, _currencyManager, _currencyManager.MINT_ROLE());
+        acl.createPermission(_voting, _airdrop, _airdrop.START_ROLE(), this);
+        acl.setPermissionManager(_voting, _airdrop, _airdrop.START_ROLE());
+    }
+
+    function _cleanup(
+        Kernel _dao, Voting _voting, TokenManager _contribManager, TokenManager _currencyManager, Tipping tipping
+    ) internal {
+        ACL acl = ACL(_dao.acl());
         bytes32 APP_MANAGER_ROLE = _dao.APP_MANAGER_ROLE();
-        bytes32 CREATE_PERMISSIONS_ROLE = _acl.CREATE_PERMISSIONS_ROLE();
-        bytes32 CURRENCY_MINT_ROLE = _currencyManager.MINT_ROLE();
-        bytes32 CURRENCY_BURN_ROLE = _currencyManager.BURN_ROLE();
+        bytes32 CREATE_PERMISSIONS_ROLE = acl.CREATE_PERMISSIONS_ROLE();
+        bytes32 MINT_ROLE = _contribManager.MINT_ROLE();
+        bytes32 BURN_ROLE = _contribManager.BURN_ROLE();
 
-        _acl.createPermission(_challenge, _currencyManager, CURRENCY_MINT_ROLE, this);
-        _acl.createPermission(_challenge, _currencyManager, CURRENCY_BURN_ROLE, this);
-        _acl.createPermission(_airdrop, _contribManager, _contribManager.MINT_ROLE(), _voting);
-        _acl.grantPermission(_airdrop, _currencyManager, CURRENCY_MINT_ROLE);
-        _acl.grantPermission(_subscribe, _currencyManager, CURRENCY_BURN_ROLE);
-        _acl.createPermission(_voting, tipping, tipping.NONE(), _voting);
+        acl.createPermission(_voting, tipping, tipping.NONE(), _voting);
 
-        _acl.createPermission(_challenge, _airdrop, _airdrop.START_ROLE(), _voting);
-        _acl.createPermission(_challenge, _subscribe, _subscribe.SET_PRICE_ROLE(), _voting);
-        _acl.createPermission(_challenge, _subscribe, _subscribe.SET_DURATION_ROLE(), _voting);
+        acl.setPermissionManager(_voting, _currencyManager, MINT_ROLE);
+        acl.setPermissionManager(_voting, _currencyManager, BURN_ROLE);
+        acl.setPermissionManager(_voting, _contribManager, MINT_ROLE);
+        acl.setPermissionManager(_voting, _contribManager, BURN_ROLE);
 
-        _acl.createPermission(ANY_ENTITY, _challenge, _challenge.PROPOSE_ROLE(), _voting);
-        _acl.createPermission(ANY_ENTITY, _challenge, _challenge.CHALLENGE_ROLE(), _voting);
-        _acl.createPermission(_voting, _challenge, _challenge.SUPPORT_ROLE(), _voting);
-        _acl.createPermission(_challenge, _challenge, _challenge.MODIFY_PARAMETER_ROLE(), _voting);
+        acl.grantPermission(_voting, _dao, APP_MANAGER_ROLE);
+        acl.revokePermission(this, _dao, APP_MANAGER_ROLE);
+        acl.setPermissionManager(_voting, _dao, APP_MANAGER_ROLE);
 
-        _acl.setPermissionManager(_voting, _currencyManager, CURRENCY_MINT_ROLE);
-        _acl.setPermissionManager(_voting, _currencyManager, CURRENCY_BURN_ROLE);
-
-        _acl.grantPermission(_voting, _dao, APP_MANAGER_ROLE);
-        _acl.revokePermission(this, _dao, APP_MANAGER_ROLE);
-        _acl.setPermissionManager(_voting, _dao, APP_MANAGER_ROLE);
-
-        _acl.grantPermission(_voting, _acl, CREATE_PERMISSIONS_ROLE);
-        _acl.revokePermission(this, _acl, CREATE_PERMISSIONS_ROLE);
-        _acl.setPermissionManager(_voting, _acl, CREATE_PERMISSIONS_ROLE);
+        acl.grantPermission(_voting, acl, CREATE_PERMISSIONS_ROLE);
+        acl.revokePermission(this, acl, CREATE_PERMISSIONS_ROLE);
+        acl.setPermissionManager(_voting, acl, CREATE_PERMISSIONS_ROLE);
     }
 
     function latestVersionAppBase(bytes32 appId) public view returns (address base) {
@@ -104,6 +144,12 @@ contract TemplateApps {
         (,base,) = repo.getLatest();
 
         return base;
+    }
+
+    function _mintHolders(TokenManager _tokenManager, address[] _holders) internal {
+        for (uint i=0; i<_holders.length; i++) {
+            _tokenManager.mint(_holders[i], 1 * 10**18); // Give 1 token to each holder
+        }
     }
 
 }
