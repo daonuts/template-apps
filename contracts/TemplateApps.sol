a045b671ecf7cb18679fb24213512b02726af77b
Carl Larson <cslarson@gmail.com>
2019-11-01 16:34:19 -0400

    move airdrop initialize to Template contract because stack too deep error

--- contracts/TemplateApps.sol
+++ contracts/TemplateApps.sol
@@ -18,6 +18,8 @@ import "@daonuts/capped-voting/contracts/CappedVoting.sol";
 contract TemplateApps {
     uint constant TOKEN_UNIT = 10 ** 18;
     address constant ANY_ENTITY = address(-1);
+    bytes32 constant MINT_ROLE = keccak256("MINT_ROLE");
+    bytes32 constant BURN_ROLE = keccak256("BURN_ROLE");
 
     bytes32 airdropDuoAppId;
     bytes32 challengeAppId;
@@ -37,7 +39,6 @@ contract TemplateApps {
         Token currency = Token(currencyManager.token());
 
         voting.initialize(contrib, currency, uint64(60 * 10**16), uint64(15 * 10**16), uint64(1 days));
-        airdrop.initialize(contribManager, currencyManager);
         challenge.initialize(
           currencyManager, 100*TOKEN_UNIT, 10*TOKEN_UNIT, 50*TOKEN_UNIT,
           uint64(1 minutes), uint64(1 minutes), uint64(1 minutes)
@@ -47,17 +48,21 @@ contract TemplateApps {
         tipping.initialize(currencyManager.token());
 
         acl.createPermission(ANY_ENTITY, harberger, harberger.PURCHASE_ROLE(), msg.sender);
-        acl.createPermission(ANY_ENTITY, harberger, harberger.MINT_ROLE(), msg.sender);
-        acl.createPermission(ANY_ENTITY, harberger, harberger.BURN_ROLE(), msg.sender);
+        acl.createPermission(ANY_ENTITY, harberger, MINT_ROLE, msg.sender);
+        acl.createPermission(ANY_ENTITY, harberger, BURN_ROLE, msg.sender);
         acl.createPermission(ANY_ENTITY, harberger, harberger.MODIFY_ROLE(), msg.sender);
-        acl.createPermission(ANY_ENTITY, contribManager, contribManager.MINT_ROLE(), msg.sender);
-        acl.createPermission(ANY_ENTITY, currencyManager, currencyManager.MINT_ROLE(), this);
-        acl.grantPermission(airdrop, currencyManager, currencyManager.MINT_ROLE());
-        acl.grantPermission(challenge, currencyManager, currencyManager.MINT_ROLE());
-        acl.createPermission(subscribe, currencyManager, currencyManager.BURN_ROLE(), this);
-        acl.grantPermission(airdrop, currencyManager, currencyManager.BURN_ROLE());
-        acl.grantPermission(challenge, currencyManager, currencyManager.BURN_ROLE());
-        acl.grantPermission(harberger, currencyManager, currencyManager.BURN_ROLE());
+
+        acl.createPermission(ANY_ENTITY, contribManager, MINT_ROLE, msg.sender);
+
+        acl.createPermission(ANY_ENTITY, currencyManager, MINT_ROLE, this);
+        acl.grantPermission(airdrop, currencyManager, MINT_ROLE);
+        acl.grantPermission(challenge, currencyManager, MINT_ROLE);
+
+        acl.createPermission(airdrop, currencyManager, BURN_ROLE, this);
+        acl.grantPermission(challenge, currencyManager, BURN_ROLE);
+        acl.grantPermission(harberger, currencyManager, BURN_ROLE);
+        acl.grantPermission(subscribe, currencyManager, BURN_ROLE);
+
         acl.createPermission(ANY_ENTITY, voting, voting.CREATE_VOTES_ROLE(), msg.sender);
         acl.createPermission(ANY_ENTITY, airdrop, airdrop.START_ROLE(), msg.sender);
         acl.createPermission(ANY_ENTITY, challenge, challenge.PROPOSE_ROLE(), msg.sender);
